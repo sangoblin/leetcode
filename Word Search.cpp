47cd2db519ed87770d16bb3e34d5b430fc21657a
Nietzsche <sangoblin@yeah.net>
2013-10-02 21:02:41 +0800

    Update Word Search.cpp

--- Word Search.cpp
+++ Word Search.cpp
@@ -10,10 +10,11 @@ class Solution {
 public:
     static const int dir[4][2];
     int m,n;
-    bool DFS(vector<vector<char> > &board, string& word, int x, int y, bool **visited)
+    bool DFS(vector<vector<char> > &board, string& word, int x, int y, vector<vector<bool> >& visited, 
+    vector<vector<Self> >& never, int depth)
     {
         if (word[0] != board[x][y]) return false;
-        //if (depth==never[x][y].depth && never[x][y].again) return false;
+        if (depth==never[x][y].depth && never[x][y].again) return false;
         char back = word[0];
         bool res = false;
         word.erase(word.begin());
@@ -24,15 +25,15 @@ public:
             int x_ = x+dir[i][0], y_ = y+dir[i][1];
             
             if (x_>=0 && x_<m && y_>=0 && y_<n && !visited[x_][y_])
-                res |= DFS(board, word, x_, y_, visited);
+                res |= DFS(board, word, x_, y_, visited, never, depth+1);
                 
             if (res)
-                //break;
                 return true;
         }
         word.insert(word.begin(), back);
-        //never[x][y].depth = depth;
-        //never[x][y].again = -1;
+        never[x][y].depth = depth;
+        never[x][y].again = -1;
+        visited[x][y] = false;
         return res;
     }
 
@@ -43,29 +44,16 @@ public:
         if (m == 0) return false;
         n = board[0].size();
         bool res = false;
+        vector<vector<bool> > visited(m, vector<bool>(n, false));
+        vector<vector<Self> > never(m, vector<Self>(n));
 
         for (int i=0; i<m; ++i)
             for (int j=0; j<n; ++j)
             {
-                bool** visited = new bool*[m*sizeof(bool*)];
-            	//Self **never = new Self*[m];
-        				for (int i=0; i<m; ++i)
-        				{
-        					visited[i] = new bool[n];
-        					//never[i] = new Self[n];
-        					memset(visited[i], 0, n*sizeof(bool));
-        					//memset(never[i], 0,);
-        				}
-                res |= DFS(board, word, i, j, visited);
+
+                res |= DFS(board, word, i, j, visited, never, 0);
                 if (res)
                     break;
-                 for (int i=0; i<m; ++i)
-                 {
-                    delete[] visited[i];
-                    //delete[] never[i];
-                 }
-                delete[] visited;
-                //delete[] never;
             }
         return res;
     }
