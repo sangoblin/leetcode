b9acff7896253028f6d8ab14d890e3bbc839001a
Nietzsche <sangoblin@yeah.net>
2013-10-02 20:45:11 +0800

    Update Gray Code.cpp

--- Gray Code.cpp
+++ Gray Code.cpp
@@ -3,18 +3,14 @@ public:
     vector<int> grayCode(int n) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if (n == 0) return vector<int>{0};
-        vector<int> res{0, 1}, power(n);
-        for (int i=0; i<n; ++i)
-            power[i] = 1<<i;
+        vector<int> res{0, 1};
             
         for (int i=1; i<n; ++i)
         {
-            vector<int> tmp = res;
-            for (auto iter=tmp.rbegin(); iter!=tmp.rend(); ++iter)
-            {
-                *iter += power[i];
-                res.push_back(*iter);
-            }
+            int len = res.size();
+
+            for (int j=len-1; j>=0; --j)
+                res.push_back(res[j]+(1<<i));
         }
         return res;
     }
