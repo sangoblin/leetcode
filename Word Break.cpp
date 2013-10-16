69fb23d6bc4a7b37f63a33209d2abbf317b5dddc
Nietzsche <sangoblin@yeah.net>
2013-10-16 11:46:26 +0800

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -1,25 +1,18 @@
 class Solution {
 public:
-    int len = 0;
-    bool wordBreakKernel(int idx, unordered_set<string>& dict, string& s, vector<bool>& state)
-    {
-       if (idx >= len)  return true;
-       if (state[idx])    return false;
-       
-       for (int i=idx; i<len; ++i)
-            if (dict.find(s.substr(idx, i-idx+1))!=dict.end() && wordBreakKernel(i+1, dict, s, state))
-                    return true;
-       
-       state[idx] = true;
-       return false;
-    }
-    
     bool wordBreak(string s, unordered_set<string> &dict) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if (s.empty())  return false;
-        len = s.size();
-        vector<bool> state(len, false);
+        int len = s.size();
+        vector<bool> state(len+1, false);
+        state[0] = true;
         
-        return wordBreakKernel(0, dict, s, state);
+        for (int i=1; i<=len; ++i)
+            for (int j=0; j<i; ++j)
+                if (state[j] && dict.find(s.substr(j, i-j))!=dict.end())
+                    state[i] = true;
+
+        
+        return state[len];
     }
 };
