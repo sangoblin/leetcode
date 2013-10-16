8f83790ceade9f9c7d029316b6e20ccadc168ebc
Nietzsche <sangoblin@yeah.net>
2013-10-16 16:13:51 +0800

    Update Clone Graph.cpp

--- Clone Graph.cpp
+++ Clone Graph.cpp
@@ -11,31 +11,31 @@ public:
     UndirectedGraphNode *cloneGraph(UndirectedGraphNode *node) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if (!node)  return nullptr;
-        map<int, UndirectedGraphNode*> record;
+        map<UndirectedGraphNode*, UndirectedGraphNode*> record;
         queue<UndirectedGraphNode *> cand;
         cand.push(node);
         UndirectedGraphNode *res = new UndirectedGraphNode(node->label);
-        record[res->label] = res;
+        record[node] = res;
 
         while (!cand.empty())
         {
             UndirectedGraphNode *cur = cand.front();
             cand.pop();
             
-            UndirectedGraphNode *tmp = record[cur->label];
+            UndirectedGraphNode *tmp = record[cur];
             
             int len = cur->neighbors.size();
                 
             for (int i=0; i<len; ++i)
-                if (record.find(cur->neighbors[i]->label) == record.end())
+                if (record.find(cur->neighbors[i]) == record.end())
                 {
                     cand.push(cur->neighbors[i]);
                     UndirectedGraphNode *tmpnbor = new UndirectedGraphNode(cur->neighbors[i]->label);
                     tmp->neighbors.push_back(tmpnbor);
-                    record[tmpnbor->label] = tmpnbor;
+                    record[cur->neighbors[i]] = tmpnbor;
                 }
                 else
-                    tmp->neighbors.push_back(record[cur->neighbors[i]->label]);
+                    tmp->neighbors.push_back(record[cur->neighbors[i]]);
         }
         
         return res;
