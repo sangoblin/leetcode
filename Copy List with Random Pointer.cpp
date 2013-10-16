9bc0e7e081b30e77d43fcf5f723641e87ff861a3
Nietzsche <sangoblin@yeah.net>
2013-10-16 16:33:09 +0800

    Create Copy List with Random Pointer.cpp

---
+++ Copy List with Random Pointer.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for singly-linked list with a random pointer.
+ * struct RandomListNode {
+ *     int label;
+ *     RandomListNode *next, *random;
+ *     RandomListNode(int x) : label(x), next(NULL), random(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    RandomListNode *copyRandomList(RandomListNode *head) {
+        // Note: The Solution object is instantiated only once and is reused by each test case.
+        map<RandomListNode*, RandomListNode*> record;
+        if (!head)  return nullptr;
+        RandomListNode *res = new RandomListNode(head->label);
+        record[head] = res;
+        
+        while (head != nullptr)
+        {
+            RandomListNode *cur = record[head];
+            
+            if (head->next == nullptr)
+            {
+                cur->random = record[head->random];
+                break;
+            }
+            
+            if (record.find(head->next) != record.end())
+                cur->next = record[head->next];
+            else
+            {
+                RandomListNode* tmp = new RandomListNode(head->next->label);
+                cur->next = tmp;
+                record[head->next] = tmp;
+            }
+            
+            if (head->random)
+            {
+                if (record.find(head->random) != record.end())
+                    cur->random = record[head->random];
+                else
+                {
+                    RandomListNode* tmp = new RandomListNode(head->random->label);
+                    cur->random = tmp;
+                    record[head->random] = tmp;
+                }
+            }
+            
+            head = head->next;
+        }
+        return res;
+    }
+};
