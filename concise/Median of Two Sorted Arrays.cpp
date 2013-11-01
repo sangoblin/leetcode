0d671655a8e14c429706444d7579edb07059a934
Nietzsche <sangoblin@yeah.net>
2013-11-01 20:22:59 +0800

    Update Median of Two Sorted Arrays.cpp

--- concise/Median of Two Sorted Arrays.cpp
+++ concise/Median of Two Sorted Arrays.cpp
@@ -1,18 +1,18 @@
-double findKth(int A[], int m, int B[], int n, int k){
-    if (m > n)  return findKth(B, n, A, m, k);
-    if (m == 0) return B[k-1];
-    if (k == 1) return min(A[0], B[0]);
-    
-    int pa = min(k/2, m);
-    int pb = k - pa;
-    
-    if (A[pa - 1] < B[pb - 1])  return findKth(A+pa, m - pa, B, n, k - pa);
-    else if (A[pa - 1] > B[pb - 1]) return findKth(A, m, B+pb, n - pb, k - pb);
-    else    return A[pa-1];
-}
-
 class Solution {
 public:
+    double findKth(int A[], int m, int B[], int n, int k){
+        if (m > n)  return findKth(B, n, A, m, k);
+        if (m == 0) return B[k-1];
+        if (k == 1) return min(A[0], B[0]);
+        
+        int pa = min(k/2, m), pb = k - pa;
+        
+        if (A[pa - 1] < B[pb - 1])  
+            return findKth(A+pa, m - pa, B, pb, k - pa);
+        else if (A[pa - 1] > B[pb - 1]) 
+            return findKth(A, pa, B+pb, n - pb, k - pb);
+        else    return A[pa-1];
+    }
     double findMedianSortedArrays(int A[], int m, int B[], int n) {
         // Start typing your C/C++ solution below
         // DO NOT write int main() function
