f9e88284d96069d2214c203fd4739c464e40ac2f
Nietzsche <sangoblin@yeah.net>
2013-11-06 10:03:51 +0800

    Create Linked List Cycle.cpp

---
+++ Linked List Cycle.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool hasCycle(ListNode *head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (nullptr == head)    return false;
+        auto slow = head, fast = head->next;
+        
+        while (slow)
+        {
+            if (fast == slow)   return true;
+            slow = slow->next;
+            if (nullptr == fast)    return false;
+            fast = fast->next;
+            if (nullptr == fast)    return false;
+            fast = fast->next;
+        }
+    }
+};
