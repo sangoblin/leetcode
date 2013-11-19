b782bbdb744e29845b6fdac90625315f8a8edfc3
Nietzsche <sangoblin@yeah.net>
2013-11-19 11:43:22 +0800

    Create Sort List.cpp

---
+++ Sort List.cpp
@@ -0,0 +1,80 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* sortListKernel(ListNode* head, int number, ListNode*& tail)
+    {
+        if (number == 1)
+        {
+            tail = head;
+            return head;
+        }
+        
+        auto head1 = sortListKernel(head, number/2, tail);
+        auto head2 = sortListKernel(tail->next, number-number/2, tail);
+        return merge(head1, number/2, head2, number-number/2, tail);
+    }
+    
+    ListNode* merge(ListNode* head1, int number1, ListNode* head2, int number2, ListNode*& tail)
+    {
+        auto dummy = new ListNode(-1), prev = dummy;
+        //int cnt1 = 0, cnt2 = 0;
+        
+        while (number1>0 && number2>0)
+        {
+            if (head1->val <= head2->val)
+            {
+                prev->next = head1;
+                prev = prev->next;
+                head1 = head1->next;
+                --number1;
+            }
+            else
+            {
+                prev->next = head2;
+                prev = prev->next;
+                head2 = head2->next;
+                --number2;
+            }
+        }
+        
+        if (number1)
+        {
+            prev->next = head1;
+            while (number1-- > 0)
+                prev = prev->next;
+        }
+
+        prev->next = head2;
+        
+        int cnt = number1+number2;
+        while (cnt-- > 0)
+            prev = prev->next;
+        tail = prev;
+        
+        auto tmp = dummy->next;
+        delete dummy;
+        return tmp;
+    }
+    
+    ListNode *sortList(ListNode *head) {
+        // IMPORTANT: Please reset any member data you declared, as
+        // the same Solution instance will be reused for each test case.
+        if (!head)  return nullptr;
+        int cnt = 0;
+        auto cur = head;
+        while (cur)
+        {
+            ++cnt;
+            cur = cur->next;
+        }
+        ListNode* tail;
+        return sortListKernel(head, cnt, tail);
+    }
+};
