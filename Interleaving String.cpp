312927cc0baac0773508d3a40a1b089e0d80b179
Nietzsche <sangoblin@yeah.net>
2013-11-22 11:51:33 +0800

    Update Interleaving String.cpp

--- Interleaving String.cpp
+++ Interleaving String.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int len1, len2;
+    int len1, len2, len3;
 
     bool isInterleave(string s1, string s2, string s3) {
         // Note: The Solution object is instantiated only once and is reused by each test case. 
         len1 = s1.size();
         len2 = s2.size();
-        //len3 = s3.size();
-        if (len1+len2 != s3.size())
-            return false;
+        len3 = s3.size();
+        if (len1+len2 != len3)  return false;
+        if (!len1)  return s2==s3;
+        if (!len2)  return s1==s3;
         vector<vector<bool> > inter(len1+1, vector<bool>(len2+1, false));
         inter[len1][len2] = true;
-        
-        int idx1 = len1, idx2 = len2;
         
         for (int i=len1; i>=0; --i)
             for (int j=len2; j>=0; --j)
