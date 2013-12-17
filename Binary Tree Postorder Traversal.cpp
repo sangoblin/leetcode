765af8fc4b58345298ec67d41662b3373d33c6f4
Nietzsche <sangoblin@yeah.net>
2013-12-17 12:20:56 +0800

    Update Binary Tree Postorder Traversal.cpp

--- Binary Tree Postorder Traversal.cpp
+++ Binary Tree Postorder Traversal.cpp
@@ -17,41 +17,26 @@ public:
         
         while (root || !post.empty())
         {
-            if (nullptr == root)
-            {
-                root = post.top();
-                post.pop();
-                if (!post.empty() && root->right==post.top())
-                {
-                    auto tmp = root;
-                    root = root->right;
-                    post.pop();
-                    post.push(tmp);
-                }
-                else
-                {
-                    res.push_back(root->val);
-                    root = nullptr;
-                    continue;
-                }
-            }
-            
-            while (root->left)
+            while (root)
             {
                 if (root->right)    post.push(root->right);
                 post.push(root);
                 root = root->left;
             }
             
-            if (!root->right)
+            root = post.top();
+            post.pop();
+            if (!post.empty() && root->right==post.top())
             {
-                res.push_back(root->val);
-                root = nullptr;
+                auto tmp = root;
+                root = root->right;
+                post.pop();
+                post.push(tmp);
             }
             else
             {
-                post.push(root);
-                root = root->right;
+                res.push_back(root->val);
+                root = nullptr;
             }
         }
         
