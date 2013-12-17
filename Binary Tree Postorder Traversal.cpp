34427be55e48cde8e5266c1e5909595416ace339
Nietzsche <sangoblin@yeah.net>
2013-12-17 12:14:32 +0800

    Update Binary Tree Postorder Traversal.cpp

--- Binary Tree Postorder Traversal.cpp
+++ Binary Tree Postorder Traversal.cpp
@@ -13,24 +13,48 @@ public:
         // IMPORTANT: Please reset any member data you declared, as
         // the same Solution instance will be reused for each test case.
         vector<int> res;
-        stack<TreeNode*> postD, postT;
-        if (root)   postT.push(root);
+        stack<TreeNode*> post;
         
-        while (!postT.empty())
+        while (root || !post.empty())
         {
-            auto cur = postT.top();
-            postT.pop();
-            postD.push(cur);
-            if (cur->left)  postT.push(cur->left);
-            if (cur->right) postT.push(cur->right);
+            if (nullptr == root)
+            {
+                root = post.top();
+                post.pop();
+                if (!post.empty() && root->right==post.top())
+                {
+                    auto tmp = root;
+                    root = root->right;
+                    post.pop();
+                    post.push(tmp);
+                }
+                else
+                {
+                    res.push_back(root->val);
+                    root = nullptr;
+                    continue;
+                }
+            }
+            
+            while (root->left)
+            {
+                if (root->right)    post.push(root->right);
+                post.push(root);
+                root = root->left;
+            }
+            
+            if (!root->right)
+            {
+                res.push_back(root->val);
+                root = nullptr;
+            }
+            else
+            {
+                post.push(root);
+                root = root->right;
+            }
         }
         
-        while (!postD.empty())
-        {
-            res.push_back(postD.top()->val);
-            postD.pop();
-        }
-
         return res;
     }
 };
