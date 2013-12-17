d28435becbca3715940b197382292271839168f4
Nietzsche <sangoblin@yeah.net>
2013-12-17 22:56:10 +0800

    Update Word Break.cpp

--- Word Break.cpp
+++ Word Break.cpp
@@ -3,16 +3,18 @@ public:
     bool wordBreak(string s, unordered_set<string> &dict) {
         // Note: The Solution object is instantiated only once and is reused by each test case.
         if (s.empty())  return false;
-        int len = s.size();
+        size_t len = s.size(), maxL = 0;
         vector<bool> state(len+1, false);
         state[0] = true;
         
-        for (int i=1; i<=len; ++i)
-            for (int j=0; j<i; ++j)
-                if (state[j] && dict.find(s.substr(j, i-j))!=dict.end())
+        for (auto& _dict : dict)
+            maxL = max(maxL, _dict.size());
+        
+        for (size_t i=1; i<=len; ++i)
+            for (size_t j=1; j<=min(maxL, i); ++j)
+                if (state[i-j] && dict.find(s.substr(i-j, j))!=dict.end())
                     state[i] = true;
 
-        
         return state[len];
     }
 };
